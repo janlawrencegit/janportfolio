8430bbd0c3c3a2a22f48ac7999fbaaf4d74600e8
Jan-Lawrence <janlawrence10@gmail.com>
2025-09-14 22:58:18 +0800

    all

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('pages.welcome');})->name('welcome');
+Route::view('/about', 'about')->name('about');
+Route::view('/portfolio', 'pages.portfolio')->name('portfolio');
+Route::view('/services', 'services')->name('services');
+Route::view('/contact', 'contact')->name('contact');
+
